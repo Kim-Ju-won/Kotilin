ee73d40e9442c9a6232ba08369b15e6d11487d8d
김주원 <kjwt1124@icloud.com>
2022-02-24 22:40:41 +0900

    Upload Day 4 Algorithm Source Code

---
+++ Mogakco-Comunity_Kotlin/Day14/MemoActivity.kt
@@ -0,0 +1,25 @@
+package com.example.secretmemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memo)
+
+        val shareButton = findViewById<FloatingActionButton>(R.id.floatingActionButton3)
+
+        shareButton.setOnClickListener {
+            val sendIntent = Intent().apply{
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "공유할 메시지")
+                type="text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+    }
+}
